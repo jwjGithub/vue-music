c6596417fbde8dd4a4bc6fe8511d03af050917ea
parker <jwj1028198397@163.com>
2020-09-08 18:42:35 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import router from './router'
 import store from './store'
 import Element from 'element-ui'
 import './assets/styles/element-variables.scss'
+import './assets/styles/index.scss' // global css
 
+Vue.prototype.baseURL = process.env.VUE_APP_BASE_API
 Vue.config.productionTip = false
 
 Vue.use(Element)

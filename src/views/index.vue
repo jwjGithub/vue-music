f009ee9ef657a8061ad2b0313e68d0763251ba2c
parker <jwj1028198397@163.com>
2020-09-18 11:52:01 +0800

    主页静态页面

--- src/views/index.vue
+++ src/views/index.vue
@@ -140,10 +140,122 @@
             </div>
           </div>
           <div class="recommend-list">
-            <div class="list"></div>
+            <div v-for="(item,index) in 8" :key="index" class="list">
+              <div class="img">
+                <img src="@/assets/images/test.jpg" />
+              </div>
+              <div class="content">
+                <div class="name">歌曲名称歌曲名称</div>
+                <div class="author">
+                  <div class="w15">
+                    <span class="title">词作者：</span>
+                    <span class="text">李珍珍</span>
+                  </div>
+                  <div class="flex-1">
+                    <span class="title">曲作者：</span>
+                    <span class="text">Lydia</span>
+                  </div>
+                </div>
+                <div class="style">
+                  <div class="style-list">风格1</div>
+                  <div class="style-list">风格2</div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+      <!-- 最新上传 -->
+      <div class="new-upload pb50">
+        <div class="new-upload-main">
+          <div class="title">
+            <div class="left">
+              <i class="icon zxsc mr15"></i>
+              <i class="icon icon-zxsc"></i>
+            </div>
+            <div class="right">
+              <span class="more">查看更多 ></span>
+            </div>
+          </div>
+          <div class="new-upload-list">
+            <table cellspacing="0" cellpadding="0" class="table-list">
+              <thead>
+                <tr>
+                  <th class="text-center w10">序号</th>
+                  <th class="text-left" style="min-width:100px;">歌曲名</th>
+                  <th class="text-left" style="min-width:100px;">词作者</th>
+                  <th class="text-left" style="min-width:100px;">曲作者</th>
+                  <th class="text-center" style="min-width:100px;">试听</th>
+                  <th class="text-center" style="min-width:100px;">购买</th>
+                  <th class="text-center" style="min-width:100px;">添加到自有库</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(item,index) in 9" :key="index">
+                  <td class="text-center">{{ index + 1 }}</td>
+                  <td class="text-left">稻香《原声》</td>
+                  <td class="text-left">周杰伦</td>
+                  <td class="text-left">周杰伦</td>
+                  <td class="text-center">
+                    <i class="icon icon-hear"></i>
+                  </td>
+                  <td class="text-center">
+                    <i class="icon icon-cart"></i>
+                  </td>
+                  <td class="text-center">
+                    <i class="icon icon-add"></i>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <footer class="footer">
+        <div class="footer-content">
+          <div class="links">
+            <div class="w12">
+              <div class="link-title">词曲库</div>
+              <div class="link-btn">词作者</div>
+              <div class="link-btn">曲作者</div>
+            </div>
+            <div class="w16">
+              <div class="link-title">Beat/BGM</div>
+              <div class="link-btn">成品伴奏</div>
+              <div class="link-btn">背景音乐</div>
+              <div class="link-btn">配乐</div>
+            </div>
+            <div class="w14">
+              <div class="link-title">榜单</div>
+              <div class="link-btn">酷狗top500</div>
+              <div class="link-btn">QQ音乐热歌榜</div>
+              <div class="link-btn">抖音热歌榜</div>
+            </div>
+            <div class="w14">
+              <div class="link-title">帮助中心</div>
+              <div class="link-btn">联系我们</div>
+              <div class="link-btn">常见问题</div>
+              <div class="link-btn">隐私政策</div>
+            </div>
+          </div>
+          <div class="contacts">
+            <div class="contacts-list">
+              <i class="icon icon-phone mr11"></i>
+              <span class="phone mr31">400-123-3333</span>
+              <i class="icon icon-wechat ml24"></i>
+              <i class="icon icon-micro-blog ml24"></i>
+            </div>
+          </div>
+        </div>
+        <div class="footer-bottom">
+          <div class="footer-bottom-text">
+            <span class="ml20">©2020~2022 曲库版权所有</span>
+            <span class="ml20">(京)-经营性-2017-0020</span>
+            <span class="ml20">京公网安备1000002000001号</span>
+            <span class="ml20">京ICP证030173号</span>
+          </div>
+        </div>
+      </footer>
     </div>
   </div>
 </template>
@@ -324,7 +436,7 @@ export default {
                 border-radius: 6px;
                 background-size: cover;
                 background-repeat: no-repeat;
-                background-image:url('~@/assets/images/index/qr-code.png');
+                background-image:url('~@/assets/images/test.jpg');
               }
               .right{
                 flex:1;
@@ -369,7 +481,7 @@ export default {
                   border-radius: 2px;
                   background-size: cover;
                   background-repeat: no-repeat;
-                  background-image:url('~@/assets/images/index/icon-wechat.png');
+                  background-image:url('~@/assets/images/test.jpg');
                 }
                 >.text{
                   flex:1;
@@ -397,7 +509,7 @@ export default {
         .recommend-main{
           width:1080px;
           margin: 0 auto;
-          .title{
+          >.title{
             height:84px;
             display:flex;
             align-items:flex-end;
@@ -415,8 +527,8 @@ export default {
                 background-image:url('~@/assets/images/index/jptj.png');
               }
               .icon-jptj{
-                width:40px;
-                height:40px;
+                width:44px;
+                height:45px;
                 background-size: cover;
                 background-position: center center;
                 background-image:url('~@/assets/images/index/icon-jptj.png');
@@ -440,8 +552,245 @@ export default {
             flex-wrap:wrap;
             justify-content: space-between;
             .list{
-
+              width:500px;
+              height: 136px;
+              // box-shadow: 0px 5px 15px 0px  rgba(255, 174, 0, 0.6);
+              border-radius: 10px;
+              display:flex;
+              // background-color: #FFF;
+              align-items:center;
+              >.img{
+                margin-left:26px;
+                margin-right:30px;
+                width: 100px;
+                height: 100px;
+                background-color: #eeeeee;
+                border-radius: 6px;
+                >img{
+                  width:100%;
+                  height:100%;
+                  border-radius: 6px;
+                }
+              }
+              >.content{
+                padding:18px 0;
+                height:calc(100% - 36px);
+                display:flex;
+                flex-direction:column;
+                justify-content: space-between;
+                >.name{
+                  font-size: 18px;
+                  color: #333333;
+                }
+                >.author{
+                  display:flex;
+                  .title{
+                    font-size: 12px;
+                    color: #999999;
+                  }
+                  .text{
+                    font-size: 14px;
+                    color: #333333;
+                  }
+                }
+                >.style{
+                  display:flex;
+                  .style-list{
+                    padding:7px 16px;
+                    margin-right:10px;
+                    border: solid 1px #cccccc;
+                    border-radius: 26px;
+                    font-size: 12px;
+                    color: #999999;
+                  }
+                }
+              }
+              &:hover{
+                cursor: pointer;
+                box-shadow: 0px 5px 15px 0px  rgba(255, 174, 0, 0.6);
+                background-color: #FFF;
+                >.content{
+                  >.style{
+                    .style-list{
+                      background-color:#fff9e2;
+                      border: 1px solid #fff9e2;
+                    }
+                  }
+                }
+              }
             }
+          }
+        }
+      }
+      // 最新上传
+      .new-upload{
+        width:100%;
+        min-height:568px;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-image:url('~@/assets/images/index/index-bg-1.png');
+        .new-upload-main{
+          width:1080px;
+          margin: 0 auto;
+          >.title{
+            height:84px;
+            display:flex;
+            align-items:flex-end;
+            justify-content:space-between;
+            padding-bottom:10px;
+            border-bottom:1px solid #EEE;
+            .left{
+              display:flex;
+              align-items:flex-end;
+              .zxsc{
+                width:120px;
+                height:29px;
+                background-size: cover;
+                background-position: center center;
+                background-image:url('~@/assets/images/index/zxsc.png');
+              }
+              .icon-zxsc{
+                width:49px;
+                height:35px;
+                background-size: cover;
+                background-position: center center;
+                background-image:url('~@/assets/images/index/icon-zxsc.png');
+              }
+            }
+            .right{
+              .more{
+                padding-right: 10px;
+                cursor: pointer;
+                font-size: 14px;
+                color: #999999;
+                &:hover{
+                  text-decoration:underline;
+                }
+              }
+            }
+          }
+          >.new-upload-list{
+            >.table-list{
+              width:100%;
+              th,td{
+                height:60px;
+                line-height:60px;
+                // display:flex;
+                // align-items:center;
+                font-size: 14px;
+              }
+              >thead{
+                th{
+                  color: #999999;
+                }
+              }
+              >tbody{
+                td{
+                  color: #333333;
+                }
+                tr:nth-child(odd){
+                  background-color: #f8f8f8;
+                }
+              }
+            }
+            .icon-hear{
+              width:24px;
+              height:16px;
+              background-size: cover;
+              background-position: center center;
+              background-image:url('~@/assets/images/index/icon-hear.png');
+            }
+            .icon-add{
+              width:19px;
+              height:18px;
+              background-size: cover;
+              background-position: center center;
+              background-image:url('~@/assets/images/index/icon-add.png');
+            }
+            .icon-cart{
+              width:23px;
+              height:20px;
+              background-size: cover;
+              background-position: center center;
+              background-image:url('~@/assets/images/index/icon-cart.png');
+            }
+          }
+        }
+      }
+      .footer{
+        width:100%;
+        background-color: #777777;
+        .footer-content{
+          width:1080px;
+          min-height:212px;
+          margin: 0 auto;
+          display:flex;
+          justify-content: space-between;
+          .links{
+            padding-top:40px;
+            display:flex;
+            .link-title{
+              font-family: PingFangSC-Regular;
+              font-size: 18px;
+              color: #ffffff;
+              margin-bottom:30px;
+            }
+            .link-btn{
+              font-family: PingFangSC-Regular;
+              font-size: 14px;
+              color: #ffffff;
+              margin-bottom:16px;
+              &:hover{
+                cursor: pointer;
+                text-decoration: underline;
+              }
+            }
+          }
+          .contacts{
+            padding-top:40px;
+            .contacts-list{
+              display:flex;
+              align-items:center;
+              .phone{
+                font-family: ArialMT;
+                font-size: 24px;
+                color: #FFF;
+              }
+              .icon-phone{
+                width:24px;
+                height:24px;
+                background-size: cover;
+                background-position: center center;
+                background-image:url('~@/assets/images/index/icon-phone.png');
+              }
+              .icon-micro-blog{
+                width:30px;
+                height:24px;
+                background-size: cover;
+                background-position: center center;
+                background-image:url('~@/assets/images/index/icon-micro-blog.png');
+              }
+              .icon-wechat{
+                width:29px;
+                height:22px;
+                background-size: cover;
+                background-position: center center;
+                background-image:url('~@/assets/images/index/icon-wechat.png');
+              }
+            }
+          }
+        }
+        .footer-bottom{
+          width:100%;
+          height: 60px;
+          border-top:1px solid #AAA;
+          .footer-bottom-text{
+            width:1080px;
+            line-height:60px;
+            margin: 0 auto;
+            font-size: 14px;
+            color: #cccccc;
           }
         }
       }
